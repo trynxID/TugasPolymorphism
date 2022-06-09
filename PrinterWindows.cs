f8188ef9890aa5240a129159553d24178adbed1c
trynxID <sidiq.firman48@gmail.com>
2022-06-09 17:32:11 +0700

    TugasPolimorphism

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    public class PrinterWindows
+    {
+        public string NamaPrinter { get; set; }
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer display dimension : ");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer printing......");
+        }
+    }
+}
